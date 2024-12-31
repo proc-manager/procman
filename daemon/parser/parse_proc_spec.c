570a0555238aeba930d0be2c6042c40e80ace7e1
rutu-sh <ruturajs10@hotmail.com>
2024-12-30 22:40:06 -0500

    fix: iterative fix

--- daemon/parser/parse_proc_spec.c
+++ daemon/parser/parse_proc_spec.c
@@ -122,7 +122,7 @@ void parse_process_yaml(char* filepath, struct Process* process) {
     printf("parsed the yaml\n");
     yaml_parser_delete(&parser);
     fclose(file);
-    print_parsed_process(*process);
+    print_parsed_process(process);
     free_process(process);
 }
 
