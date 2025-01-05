d9bfde493e17928a2ef7bea955a29cb34e372aaa
rutu-sh <ruturajs10@hotmail.com>
2025-01-05 06:32:47 +0000

    fix: iterative fix

--- daemon/src/lib/isoproc.h
+++ daemon/src/lib/isoproc.h
@@ -8,5 +8,7 @@ int isoproc(void *p);
 
 
 void prepare_mntns(struct Process* proc);
+void overwrite_env(struct Process* proc);
+void execute_job(struct Process* proc);
 
 #endif // ISOLATED_PROCESS_H
